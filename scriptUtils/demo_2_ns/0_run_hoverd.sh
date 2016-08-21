a366c00e724b6e4dd3631f88cd2678386d099600
Bertrone Matteo <m.bertrone@gmail.com>
2016-08-21 13:08:57 +0200

    delete old files

--- scriptUtils/demo_2_ns/0_run_hoverd.sh
+++
@@ -1,2 +0,0 @@
-go install github.com/mbertrone/iomodules/hover/hoverd
-sudo $GOPATH/bin/hoverd -listen 127.0.0.1:5002
--- scriptUtils/demo_2_ns/1_run_politod.sh
+++
@@ -1,3 +0,0 @@
-go install github.com/mbertrone/politoctrl/politod
-sudo $GOPATH/bin/politod -hover http://127.0.0.1:5002 
-#-port1 i:veth1_ -port2 i:veth2_
