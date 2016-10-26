9e5a6f82bb446e587748898a0e4a758718f4b4a3
Bertrone Matteo <m.bertrone@gmail.com>
2016-10-25 21:01:55 -0700

    demo Switch security ports updated

--- examples/demoSwitchSecurityPorts/switchSecurityPortsMacIp.sh
+++ examples/demoSwitchSecurityPorts/switchSecurityPortsMacIp.sh
@@ -5,19 +5,11 @@
 # Setup Ns
 #
 # Create a Switch
-# Connect 7 ports with Security Ports on MAC and IP
+# Connect 4 ports with Security Ports on MAC and IP
 # Test Ping works
 #
-# Configure Ports Security
-# p1 no policy              OK
-# p2 mac+ip correct policy  OK
-# p3 mac correct            OK
-# p4 ip correct             OK
-# p5 mac wrong              NO
-# p6 ip wrong               NO
-# p7 MAC + IP wrong         NO
-
-n=7
+#Change mac/IP in namespaces 3-4 -> mac and IP not matching Rules anymore
+n=4
 
 echo "cleanup previous namespaces"
 for i in `seq 1 $n`;
@@ -31,6 +23,8 @@ do
   ovs-vsctl --no-wait del-port br-int veth${i}_
   ovn-nbctl lsp-del sw0-port${i}
 done
+
+ovn-nbctl ls-del sw0
 
 sleep 1
 
@@ -54,49 +48,28 @@ do
   mac=(${mac[@]} $new_mac)
 done
 
-echo "adding logical-switch"
+echo "ovn-nbctl ls-add sw0"
 ovn-nbctl ls-add sw0
 
 for i in `seq 1 $n`;
 do
-  echo "adding logical-switch sw0 port sw0-port${i}"
+  echo "ovn-nbctl lsp-add sw0 sw0-port${i}"
   ovn-nbctl lsp-add sw0 sw0-port${i}
+  echo ovn-nbctl lsp-set-port-security sw0-port${i} "${mac[$i]} 10.10.1.${i}"
   ovn-nbctl lsp-set-port-security sw0-port${i} "${mac[$i]} 10.10.1.${i}"
+  echo ovs-vsctl --no-wait add-port br-int veth${i}_ -- set Interface veth${i}_ external_ids:iface-id=sw0-port${i}
   ovs-vsctl --no-wait add-port br-int veth${i}_ -- set Interface veth${i}_ external_ids:iface-id=sw0-port${i}
 done
 
-echo "**SWITCH 7 ports with Ports security on MAC and IP**"
+echo ""
+echo "**CURRENT CONFIGURATION**"
+echo "Switch 4 Ports"
+echo "Port Security on MAC and IP"
+echo ""
+echo "In current configuration Ping Should Work for each interface"
+echo ""
 echo "PRESS A KEY TO START PING....."
 read -r line
-
-# sudo ip netns exec ns2 ping 10.10.1.1 -c 1
-
-for i in `seq 2 $n`;
-do
-  sudo ip netns exec ns1 ping 10.10.1.${i} -c 1
-done
-
-echo "PRESS A KEY TO CHANGE CONFIG....."
-
-echo "**SWITCH 7 ports with Ports security on MAC and IP**"
-echo "Port1 No Policy->OK"
-echo "Port2 Policy MAC +IP Correct->OK"
-echo "Port3 MAC Correct->OK"
-echo "Port4 IP Correct->OK"
-echo "Port5 MAC Wrong->KO!"
-echo "Port6 IP Wrong->KO!"
-echo "Port7 MAC+IP Wrong->KO!"
-
-read -r line
-
-ovn-nbctl lsp-set-port-security sw0-port1 ""
-ovn-nbctl lsp-set-port-security sw0-port2 "${mac[2]} 10.10.1.2"
-ovn-nbctl lsp-set-port-security sw0-port3 "${mac[3]} "
-ovn-nbctl lsp-set-port-security sw0-port4 "10.10.1.4"
-ovn-nbctl lsp-set-port-security sw0-port5 "01:00:02:00:03:00"
-ovn-nbctl lsp-set-port-security sw0-port6 "10.1.2.33"
-ovn-nbctl lsp-set-port-security sw0-port7 "00:11:00:22:00:33 10.1.2.77"
-
 
 sudo ip netns exec ns2 ping 10.10.1.1 -c 1
 
@@ -105,6 +78,52 @@ do
   sudo ip netns exec ns1 ping 10.10.1.${i} -c 1
 done
 
+echo ""
+echo "Ping END"
+echo ""
+echo ""
+echo "PRESS A KEY TO CHANGE CONFIG (change some ip address inside ns)....."
+read -r line
+
+echo ""
+echo "sudo ip netns exec ns3 ifconfig veth3 10.10.1.103/24"
+sudo ip netns exec ns3 ifconfig veth3 10.10.1.103/24
+echo "sudo ip netns exec ns4 ifconfig veth4 10.10.1.104/24"
+sudo ip netns exec ns4 ifconfig veth4 10.10.1.104/24
+
+
+echo ""
+echo "**CURRENT CONFIGURATION**"
+echo "Switch 4 Ports"
+echo "Port Security on MAC and IP"
+echo "Port3 -> WRONG IP -> Switch will DROP Packets"
+echo "Port4 -> WRONG IP -> Switch will DROP Packets"
+echo ""
+echo "In current configuration Ping from/to ns3-4 should NOT work!"
+echo ""
+echo "PRESS A KEY TO START PING....."
+read -r line
+
+echo "PING from: ns1 --> to: ns2 (10.10.1.2)  OK!"
+echo ""
+sudo ip netns exec ns1 ping 10.10.1.2 -c 1
+
+echo "PING from: ns2 --> to: ns1 (10.10.1.1)  OK!"
+echo ""
+sudo ip netns exec ns2 ping 10.10.1.1 -c 1
+
+echo "PING from: ns3 --> to: ns1 (10.10.1.1)  DROP"
+echo ""
+sudo ip netns exec ns3 ping 10.10.1.1 -c 1
+
+echo "PING from: ns4 --> to: ns1 (10.10.1.1)  DROP"
+echo ""
+sudo ip netns exec ns4 ping 10.10.1.1 -c 1
+
+echo ""
+echo "Ping END"
+echo ""
+echo ""
 echo "PRESS A KEY TO CLEANUP....."
 read -r line
 echo "OVN Cleanup"
