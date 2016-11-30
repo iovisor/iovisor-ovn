3ec65788e49726061abfb30da50fab998883efb0
Mauricio Vasquez B <mauricio.vasquez@polito.it>
2016-11-30 12:12:36 -0500

    iomodules: add IoModules interface

--- iomodules/iomodules.go
+++ iomodules/iomodules.go
@@ -1,1 +1,10 @@
 package iomodules
+
+type IoModule interface {
+	Deploy() (err error)
+	Destroy() (err error)
+	AttachExternalInterface(name string) (err error)
+	DetachExternalInterface(name string) (err error)
+	AttachToIoModule(IfaceId int, name string) (err error)
+	DetachFromIoModule(name string) (err error)
+}
