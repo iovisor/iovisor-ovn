25b9ae16890fb5005d17f2e5403edd1db60b46a8
Mauricio Vasquez B <mauricio.vasquez@polito.it>
2017-01-27 13:22:40 -0500

    examples/switch: remove interfaces before creating them

--- examples/switch/setup.sh
+++ examples/switch/setup.sh
@@ -3,6 +3,8 @@
 for i in `seq 1 2`;
 do
 	sudo ip netns del ns${i}	# remove ns if already existed
+	sudo ip link del veth${i}
+
 	sudo ip netns add ns${i}
 	sudo ip link add veth${i}_ type veth peer name veth${i}
 	sudo ip link set veth${i}_ netns ns${i}
